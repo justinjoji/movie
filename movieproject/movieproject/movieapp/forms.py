a46020b5bf43e9019cc069c3cd1af28fbb432ca2
justin joji <justinjoji1228@gmail.com>
2022-12-26 11:07:04 +0530

    added

---
+++ movieproject/movieproject/movieapp/forms.py
@@ -0,0 +1,10 @@
+import form as form
+from django import forms
+from .models import Movie
+
+
+
+class MovieForm(forms.ModelForm):
+    class Meta:
+        model=Movie
+        fields=['name','desc','year','img']
